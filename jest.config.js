9b80b7edbdea3503580e3e4efc78b41753e5c5cd
pvmelli <paula.v.melli@gmail.com>
2020-06-06 17:59:48 -0300

    Added jest to dependencies

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    verbose: true,
+    rootDir: 'src',
+    coverageDirectory: '../coverage/',
+    testPathIgnorePatterns: ['/node_modules/', '.*fixture.js'],
+    coveragePathIgnorePatterns: ['/node_modules/', '.*fixture.js'],
+};
