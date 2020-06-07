3d4823af226745bcf4bbc9f4b75557f334ac856b
pvmelli <paula.v.melli@gmail.com>
2020-06-07 15:53:26 -0300

    Added instructions

--- src/calendar.js
+++ src/calendar.js
@@ -1,7 +1,10 @@
+import {showInstructions} from './ui/general.js';
 import {manageWeeklyView, getWeekDays} from './ui/calendar.js';
 import {getEvents} from './services/service.js';
 
 export function initialize() {
+
+    showInstructions();
 
     manageWeeklyView(getWeekDays, getEvents)
 
--- src/ui/general.js
+++ src/ui/general.js
@@ -1,3 +1,16 @@
+export function showInstructions() {
+
+    if(localStorage.length === 0) {
+        const $instructions = document.querySelector('#instructions');
+        $instructions.classList.remove('not-display');
+    
+        const $closeInstructionsButton = document.querySelector('#close-instructions-button');
+        $closeInstructionsButton.addEventListener('click', () => {$instructions.classList.add('not-display')})
+    }else{
+        return 'This is not your fist time visiting this page'
+    }
+}
+
 export function getSelectedWeek() {
     const weekOfTheYear = document.querySelector('#week-input').value;
     const year = weekOfTheYear.slice(0,4)
