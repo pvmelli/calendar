194fad8c95a52a2ac71ec0d6c2fb13dd800792a6
pvmelli <paula.v.melli@gmail.com>
2020-06-07 16:30:38 -0300

    Revert "Resolved"
    This reverts commit 9ff399cd41ed84253400d7f4938a49e88eb44b6c.

--- src/calendar.js
+++ src/calendar.js
@@ -1,7 +1,6 @@
 import {showInstructions} from './ui/general.js';
 import {manageWeeklyView, getWeekDays} from './ui/calendar.js';
 import {getEvents} from './services/service.js';
-import {activateNewEventModal} from './ui/new-event.js'
 
 export function initialize() {
 
@@ -11,9 +10,6 @@ export function initialize() {
 
     const $weekInput = document.querySelector('#week-input');
     $weekInput.addEventListener('input', () => {manageWeeklyView(getWeekDays, getEvents)})
-
-    const $newEventButton = document.querySelector('#new-event-button');
-    $newEventButton.addEventListener('click', () => {activateNewEventModal()})
 
     return 'Initialization complete';
 
